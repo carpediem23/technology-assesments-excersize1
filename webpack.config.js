9500ed3fa28e4a38b3c8dc1c1d0a37c79ebd3fc7
Alptuğ Türkiş <carpediem.2323@gmail.com>
2022-03-30 20:44:05 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,51 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 require('dotenv').config();
 
 module.exports = {
   entry: './src/index.js',
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.[fullhash].js'
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './public/index.html'
-    })
-  ],
+  resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader']
       }
     ]
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new WebpackManifestPlugin({
+      fileName: 'manifest.json'
+    })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'public')
     },
     compress: true,
     port: process.env.PORT || 3000,
-    allowedHosts: ['.gitpod.io']
+    allowedHosts: ['.gitpod.io'],
+    historyApiFallback: true,
+    open: true
   }
 };
