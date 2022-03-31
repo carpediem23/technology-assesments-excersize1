d70cbc59583faddc7b7214624ea1a41390e2d8ec
Alptuğ <carpediem.2323@gmail.com>
2022-03-31 11:57:08 +0000

    Test entegre edildi.

---
+++ __tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+
+test('App rendering', () => {
+  const component = renderer.create(
+    <App />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
