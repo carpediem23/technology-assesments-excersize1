9ad9ae0d4328e71207cf845890b62635d9f1137e
Alptuğ Türkiş <carpediem.2323@gmail.com>
2022-03-31 20:27:14 +0300

    css minify eklendi.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 require('dotenv').config();
 
@@ -21,7 +23,14 @@ module.exports = {
       },
       {
         test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true } },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
       },
       {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
@@ -32,19 +41,27 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "./public/manifest.webmanifest", to: "." },
-        { from: "./public/icon.png", to: "assets/" },
-      ],
+        { from: './public/manifest.webmanifest', to: '.' },
+        { from: './public/icon.png', to: 'assets/' }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[chunkhash].css'
     }),
     new HTMLWebpackPlugin({
       inject: true,
       template: './public/index.html',
-      filename: "index.html",
+      filename: 'index.html',
       favicon: './public/favicon.ico',
       publicPath: '.',
-      manifest: './public/manifest.json',
+      manifest: './public/manifest.json'
     })
   ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin()
+    ]
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'public')
